a4dc511680ba8f73e9255032d1c678e65337f908
Dante Schiavi <dantemds@hotmail.com>
2026-05-12 17:45:03 -0300

    Adicionando autorização para a BCR CX

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ CORS(app)  # Permite CORS de qualquer origem (para teste)
 ALLOWED_ORIGINS = {
     "https://conecta.bcrcx.com",
     "https://institucional.bcrcx.com",
+    "https://consultoria.bcrcx.com"
 }
 
 CORS(
